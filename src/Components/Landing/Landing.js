2085caef5ae997878e0b28acd4fecf5ec866c24f
adeelzahid <adeelzahid94@gmail.com>
2023-03-29 17:01:34 -0700

    code to create supabase client for DB connection

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,11 +3,18 @@ import './Landing.css';
 import LandingTopBar from "../LandingTopBar/LandingTopBar";
 import LandingMainBar from "../LandingMainBar/LandingMainBar";
 
-export default function Landing () {
+//import the supabase client
+import supabase from "../../Config/supabaseClient";
+
+export default function Landing() {
+
+    //log the supbase client to the console
+    console.log(supabase);
+
     return (
-        <div id="grid-container"> 
-            <LandingTopBar id="landing-top-bar"/>
-            <LandingMainBar id="landing-main-bar"/>
+        <div id="grid-container">
+            <LandingTopBar id="landing-top-bar" />
+            <LandingMainBar id="landing-main-bar" />
         </div>
     );
 }
