96f14cb1c201df134087d7ea2da2f4f01bda271a
adeelzahid <adeelzahid94@gmail.com>
2023-04-11 14:43:56 -0700

    save user sign up data in the DB

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -7,7 +7,7 @@ import './SignUpForm.css';
 import { FilledInContext } from "../SignUpScreen/SignUpScreen";
 
 
-export default function SignUpForm () {
+export default function SignUpForm() {
 
     const [first_name, set_first_name] = useState("");
     const [last_name, set_last_name] = useState("");
@@ -18,92 +18,107 @@ export default function SignUpForm () {
     const [confirm_password, set_confirm_password] = useState("");
     const [show_password, set_show_password] = useState(false);
     const [show_confirm_password, set_show_confirm_password] = useState(false);
-    const [filled_in, set_filled_in] = useContext(FilledInContext);
 
-    function check_all_fields () {
+
+    //we use the useContext hook to access the values from the FilledInContext object
+    const { form_data, set_form_data, filled_in, set_filled_in } = useContext(FilledInContext);
+
+    function check_all_fields() {
         return (
-            first_name != "" && 
-            last_name != "" && 
-            email != "" && 
+            first_name != "" &&
+            last_name != "" &&
+            email != "" &&
             confirm_email != "" &&
-            username != "" && 
-            password != "" && 
+            username != "" &&
+            password != "" &&
             confirm_password != ""
-            )
+        )
     }
 
     return (
         <>
-            <Image id="blank-card-image" src={BlankCard}/>
+            <Image id="blank-card-image" src={BlankCard} />
 
             <div id="signup-form-group">
                 <FormGroup>
 
                     <span id="first-name-text"> First Name </span>
-                    <InputGroup 
+                    <InputGroup
                         id="signup-form-first-name"
                         placeholder="Text"
                         onChange={(event) => {
                             console.log(event.target.value);
                             set_first_name(event.target.value);
                             set_filled_in(check_all_fields());
-
+                            //pass the data to the SignUpScreen component using set_form_data method from the FilledInContext object
+                            set_form_data({ ...form_data, first_name: event.target.value });
                         }}
                     />
 
                     <span id="last-name-text"> Last Name </span>
-                    <InputGroup 
+                    <InputGroup
                         id="signup-form-last-name"
                         placeholder="Text"
                         onChange={(event) => {
                             set_last_name(event.target.value);
                             set_filled_in(check_all_fields())
+                            //pass the data to the SignUpScreen component using set_form_data method from the FilledInContext object
+                            set_form_data({ ...form_data, last_name: event.target.value });
+
                         }}
                     />
 
                     <span id="email-text"> Email </span>
-                    <InputGroup 
+                    <InputGroup
                         id="signup-form-email"
                         placeholder="Text"
                         onChange={(event) => {
                             set_email(event.target.value);
                             set_filled_in(check_all_fields())
+                            //pass the data to the SignUpScreen component using set_form_data method from the FilledInContext object
+                            set_form_data({ ...form_data, email: event.target.value });
                         }}
                     />
 
                     <span id="confirm-email-text"> Confirm Email </span>
-                    <InputGroup 
+                    <InputGroup
                         id="signup-form-confirm-email"
                         placeholder="Text"
                         onChange={(event) => {
                             set_confirm_email(event.target.value);
                             set_filled_in(check_all_fields())
+
                         }}
                     />
 
                     <span id="username-text"> Username </span>
-                    <InputGroup 
+                    <InputGroup
                         id="signup-form-username"
                         placeholder="Text"
                         onChange={(event) => {
                             set_username(event.target.value);
                             set_filled_in(check_all_fields())
+                            //pass the data to the SignUpScreen component using set_form_data method from the FilledInContext object
+                            set_form_data({ ...form_data, username: event.target.value });
                         }}
                     />
 
                     <span id="password-text"> Password </span>
-                    <InputGroup 
+                    <InputGroup
                         id="signup-form-password"
                         placeholder="Text"
                         onChange={(event) => {
                             set_password(event.target.value);
                             set_filled_in(check_all_fields())
+                            //pass the data to the SignUpScreen component using set_form_data method from the FilledInContext object
+                            set_form_data({ ...form_data, password: event.target.value });
+
                         }}
                         type={show_password ? "password" : "text"}
                     />
 
                     <span id="confirm-password-text"> Confirm Password </span>
-                    <InputGroup 
+                    <InputGroup
                         id="signup-form-confirm-password"
                         placeholder="Text"
                         round={true}
@@ -112,44 +127,44 @@ export default function SignUpForm () {
                             set_filled_in(check_all_fields())
                         }}
                         type={show_confirm_password ? "password" : "text"}
-                        
+
                     />
 
-                    {!show_password && 
-                    <Icon 
-                        id="password-unlock-icon"
-                        icon="unlock"
-                        onClick={() => {
-                            set_show_password((prev) => !prev)
-                        }}
-                    />}
+                    {!show_password &&
+                        <Icon
+                            id="password-unlock-icon"
+                            icon="unlock"
+                            onClick={() => {
+                                set_show_password((prev) => !prev)
+                            }}
+                        />}
 
-                    {show_password && 
-                    <Icon 
-                        id="password-lock-icon"
-                        icon="lock"
-                        onClick={() => {
-                            set_show_password((prev) => !prev)
-                        }}
-                    />}
+                    {show_password &&
+                        <Icon
+                            id="password-lock-icon"
+                            icon="lock"
+                            onClick={() => {
+                                set_show_password((prev) => !prev)
+                            }}
+                        />}
 
-                    {!show_confirm_password && 
-                    <Icon 
-                        id="confirm-password-unlock-icon"
-                        icon="unlock"
-                        onClick={() => {
-                            set_show_confirm_password((prev) => !prev)
-                        }}
-                    />}
+                    {!show_confirm_password &&
+                        <Icon
+                            id="confirm-password-unlock-icon"
+                            icon="unlock"
+                            onClick={() => {
+                                set_show_confirm_password((prev) => !prev)
+                            }}
+                        />}
 
-                    {show_confirm_password && 
-                    <Icon 
-                        id="confirm-password-lock-icon"
-                        icon="lock"
-                        onClick={() => {
-                            set_show_confirm_password((prev) => !prev)
-                        }}
-                    />}
+                    {show_confirm_password &&
+                        <Icon
+                            id="confirm-password-lock-icon"
+                            icon="lock"
+                            onClick={() => {
+                                set_show_confirm_password((prev) => !prev)
+                            }}
+                        />}
                 </FormGroup>
             </div>
         </>
--- src/Components/SignUpScreen/SignUpScreen.js
+++ src/Components/SignUpScreen/SignUpScreen.js
@@ -9,39 +9,81 @@ import { Image } from "react-bootstrap";
 import CreateAccountButton from "../../Images/CreateAccountButton.svg";
 import FilledCreateAccount from '../../Images/FilledCreateAccount.svg';
 import NotFilledCreateAccount from '../../Images/NotFilledCreateAccount.svg';
+//importing the supabase client
+import supabase from '../Config/dbconnection';
 
 
-
+//create a context object to pass data between SignUpScreen and SignUpForm
 export const FilledInContext = createContext();
+
 
 export default function SignUpScreen() {
 
+    //filled_in is a boolean that is true if all fields are filled in
     const [filled_in, set_filled_in] = useState(false);
-    
+
+    //declare another state variable to store the form data for the user
+    const [form_data, set_form_data] = useState({ first_name: '', last_name: '', email: '', username: '', password: '' });
+
+    //create a function to insert the data into the database
+    const saveToDB = async (e) => {
+        console.log("Saving Sign up data to DB");
+        console.log(filled_in)
+        console.log(form_data)
+        e.preventDefault(); //prevent the page from refreshing when the user clicks the submit button
+
+        // //check if we actually have data in the state variables
+        // console.log(email, age, interest);
+
+        // if (!email || !age || !interest) {
+        //     alert('Please enter all fields')
+        //     return
+        // }
+
+        //convert the interest array into a format that can be inserted into the post gres database
+        //setInterest(JSON.stringify(interest).replace('[', '{').replace(']', '}')) //check why this line is not working for proper formatting
+
+        //make a call to the users table and insert the data into the table
+        let { data: new_user, error } = await supabase //data now would be the new row that was inserted into the table if insert was successful
+
+            .from('user_data')
+            .insert([
+                { first_name: form_data['first_name'], last_name: form_data['last_name'], email_address: form_data['email'], user_name: form_data['username'], password: form_data['password'] }
+            ])
+
+        //if there is an error, log it to the console
+        if (error) console.log('error', error)
+        else
+            console.log("Data successfully saved to DB")
+    }
+
+
+    //combine form_data and filled_in into an object to pass to the FilledInContext object
+    const form_details = { form_data, set_form_data, filled_in, set_filled_in }
 
     return (
         <>
             <div id="signup-top-bar">
-                <LandingTopBar/>
+                <LandingTopBar />
             </div>
 
-            <FilledInContext.Provider value={[filled_in, set_filled_in]}>
+            <FilledInContext.Provider value={form_details}>
                 <div id="signup-form">
-                    <SignUpForm/>
+                    <SignUpForm />
                 </div>
             </FilledInContext.Provider>
 
-            <div id="create-account">
+            <div id="create-account" onClick={saveToDB}>
                 <Button id="create-account-button">
-                    {filled_in && <Image id="create-account-button-image" src={FilledCreateAccount}/>}
-                    {!filled_in && <Image id="create-account-button-image" src={NotFilledCreateAccount}/>}
+                    {filled_in && <Image id="create-account-button-image" src={FilledCreateAccount} />}
+                    {!filled_in && <Image id="create-account-button-image" src={NotFilledCreateAccount} />}
                     {/* {!filled_in && <h3 id="create-account-text-not-filled"> Create Account </h3>}
                     {filled_in && <h3 id="create-account-text-filled"> Create Account </h3>} */}
                 </Button>
             </div>
 
             <div id="signup-forest">
-                <Image id="signup-forest-image" src={SignUpForest}/>
+                <Image id="signup-forest-image" src={SignUpForest} />
             </div>
         </>
     )
